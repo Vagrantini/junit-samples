421e6b7891eb1174123a59754cfd5d68550915af
Feng Zhang <fezhang@thoughtworks.com>
2020-02-06 15:35:50 +0800

    init spring boot project

---
+++ src/main/java/com/evan/junit/JunitSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.evan.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evan/junit/JunitSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evan.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
